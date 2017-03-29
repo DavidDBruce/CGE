5470588b68dd816159e9d7ff16454260cf306c53
Tarunkumar-dev <s528757@mail.nwmissouri.edu>
2017-03-29 17:28:49 -0500

    is deleted add for roofing topcoats

--- models/roofingTopcoat.js
+++ models/roofingTopcoat.js
@@ -15,7 +15,8 @@ var RoofingTopcoatSchema = new Schema({
     name:  { type: String, required: true },
     unit:  { type: String, required: true },
     displayorder:  { type: String, required: true },
-    price:  { type: Number, required: true }
+    price:  { type: Number, required: true },
+    isDeleted:{type: boolean, required: true }
 })
 
 var roofingTopcoat = mongoose.model('RoofingTopcoat', RoofingTopcoatSchema)
