59d15c233962b97c614b37b094b0d0a7c35fe27f
AvinashVasadi <S528770@mail.nwmissouri.edu>
2017-03-28 20:09:10 -0500

    Relevant comments updated

--- models/waterproofingTopcoat.js
+++ models/waterproofingTopcoat.js
@@ -1,8 +1,7 @@
 
 //p4-11 Avinash Vasadi, Darshan Yadav Venkatesh.
 
-
-   Schema = mongoose.Schema
+Schema = mongoose.Schema
 
 var WaterproofingTopcoatSchema = new Schema({
     name:  { type: String, required: true },
