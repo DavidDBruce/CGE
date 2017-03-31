29817c515afd93439c6d6f197099e9b283153af5
S528763 <S528763@MAIL.NWMISSOURI.EDU>
2017-03-30 21:52:22 -0500

    P4-7 Team changes

--- controllers/waterproofingEstimate.js
+++ controllers/waterproofingEstimate.js
@@ -1,4 +1,3 @@
-// P4-7 | Aditya Srimat Tirumala Pallerlamudi, Lavanya Kalluri
 // P4-5 
 // Saidevi Uppalapati , Raviteja Chintala
 // p4-10 | Aswini Vadlamudi, Haritha Kurla 
@@ -142,30 +141,3 @@ api.post('/delete/:id', function(req, res, next) {
 });
 
 module.exports = api;
-
-
-
-
-/* 10 controller methods handled by controller:
-
-controllers/waterproofingEstimate.js
-
-2 Respond with JSON:
-
-http://localhost:8082/waterproofingEstimate/findall [WORKING]
-http://localhost:8082/waterproofingEstimate/findone/1 [WORKING]
-
-5 Respond with CRUD Views:
-
-http://localhost:8082/waterproofingEstimate [WORKING]
-http://localhost:8082/waterproofingEstimate/create [WORKING]
-http://localhost:8082/waterproofingEstimate/delete/1 [WORKING]
-http://localhost:8082/waterproofingEstimate/details/1 [WORKING]
-http://localhost:8082/waterproofingEstimate/edit/1[WORKING]
-
-3 Respond by executing CRUD actions:
-
-http://localhost:8082/waterproofingEstimate/save [NOT-WORKING]
-http://localhost:8082/waterproofingEstimate/save/1 [NOT-WORKING]
-http://localhost:8082/waterproofingEstimate/delete/1
-*/
