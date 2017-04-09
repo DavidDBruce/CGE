9a5ef3225d5c37da6d559ab2a2cc9a28c4390745
Jordan Nazario <Jordan@david-s-windows-phone.nwmissouri.edu>
2017-04-09 13:30:32 -0500

    More work on adding prospect model

--- controllers/prospects.js
+++ controllers/prospects.js
@@ -2,6 +2,11 @@
 
 var express = require('express');
 var api = express.Router();
+var find = require('lodash.find');
+var remove = require('lodash.remove');
+var findIndex = require('lodash.findindex');
+var Model = require('../models/prospect.js');
+const notfoundstring = 'No prospects';
 
 
 api.get("/", function (req,res) {
