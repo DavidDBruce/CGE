c86ca9a301a2911bdcdf39ecb3b4674ac6e0128f
Kmott94 <kenneth.n.mott@gmail.com>
2017-04-16 14:27:56 -0500

    Commit to fix issue with adding new entry
    Desc did not display the new rate

--- models/mileageRate.js
+++ models/mileageRate.js
@@ -11,3 +11,21 @@ var MileageRateSchema = new Schema({
 var mileageRate = mongoose.model('MilegeRate', MileageRateSchema)
 module.exports = mileageRate
 
+//*** This code does not work at the moment but does not affect the project
+// var request = require('request'),
+//   cheerio = require('cheerio'),
+//   mileRate = "";
+
+// function getMileageRate(){
+// request('https://www.irs.gov/tax-professionals/standard-mileage-rates', function(err, resp, body){
+//   if(!err && resp.statusCode ==200){
+//     var $ = cheerio.load(body);
+//     $('first-child', '#content').each(function(){
+//       var mileRate = this.attr('href');
+//       mileRate.push(mileRate);
+
+//     });
+//   }
+// });   
+// return mileRate;
+// }
