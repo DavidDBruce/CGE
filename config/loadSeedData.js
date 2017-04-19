26384ece47045318d1eed2db86c62b070352ba33
sravanthi <s528749@mail.nwmissouri.edu>
2017-04-19 14:36:30 -0500

    team hoses - created controller and updated loadseeddata

--- config/loadSeedData.js
+++ config/loadSeedData.js
@@ -27,7 +27,7 @@ module.exports = {
         
         // global.rollers = require('../data/waterproofingTopcoats.yml');
         // global.buckets = require('../data/waterproofingTopcoats.yml');
-        // global.hoses = require('../data/waterproofingTopcoats.yml');
+        global.hoses = require('../data/waterproofingTopcoats.yml');
         // global.brooms = require('../data/waterproofingTopcoats.yml');
 
         // global.ladders = require('../data/waterproofingTopcoats.yml');
@@ -60,7 +60,7 @@ module.exports = {
 
         // db.insert(rollers);
         // db.insert(buckets);
-        // db.insert(hoses);
+        db.insert(hoses);
         // db.insert(brooms);
 
         // db.insert(ladders);
@@ -94,7 +94,7 @@ module.exports = {
 
         // app.locals.rollers = db.find(rollers);
         // app.locals.buckets   = db.find(buckets);
-        // app.locals.hoses  = db.find(hoses);
+         app.locals.hoses  = db.find(hoses);
         // app.locals.brooms       = db.find(brooms);
 
         // app.locals.ladders          = db.find(ladders);
