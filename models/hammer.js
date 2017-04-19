eaaa21ddeb832cdb51171c19176bcc4004226d95
s528760 <s528760@mail.nwmissouri.edu>
2017-04-19 13:16:25 -0500

    hammer model

---
+++ models/hammer.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
+var HammerSchema = new Schema({
+  _id: { type: Number, required: true },
+  name: { type: String, required: true },
+  displayorder: { type: String, required: true },
+  price: { type: Number, required: true },
+  isActive:{type: Boolean, required: true, default:true }
+})
+
+var hammer = mongoose.model('RoofingBasecoat', HammerSchema)
+module.exports = hammer
