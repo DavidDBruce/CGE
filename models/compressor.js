1994f207de9f6af05a21a730868b04c82363c0c9
NWMSU\S528811 <S528811@mail.nwmissouri.edu>
2017-04-19 16:31:36 -0500

    models and data for compressor added

---
+++ models/compressor.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
+var compressorSchema = new Schema({
+  _id: { type: Number, required: true },
+  name: { type: String, required: true },
+  
+  displayorder: { type: Number, required: true },
+  price: { type: Number, required: true },
+  isdeleated : { type : Boolean , required : true }
+})
+
+var compressor = mongoose.model('compressor', compressorSchema)
+module.exports = compressor
+// Team Compressor (Venkatesh Darshan Yadav,Kolla Venkata Naveen Kumar, Nithya )
